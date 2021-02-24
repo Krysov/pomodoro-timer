be8572fcaeca4776da27355b14f6e1f0a466b4a6
krysov <>
2021-02-24 20:46:32 +0100

    + added meat to CountdownTimer; + added timer test tolerance where needed; ~ making test pass;

--- src/CountdownTimer.test.ts
+++ src/CountdownTimer.test.ts
@@ -1,6 +1,13 @@
 import CountdownTimer from './CountdownTimer';
 
+const TIMER_TEST_PRECISION_MS = 100;
+
 describe('CountdownTimer', () => {
+
+    function expectCloseTo(received : number, expected: number, tolerance : number ) : void {
+        expect(received).toBeLessThanOrEqual(expected + tolerance);
+        expect(received).toBeGreaterThanOrEqual(expected - tolerance);
+    }
 
     it('test value assignment', async () => {
         // time on setup should be uninitialized
@@ -33,26 +40,27 @@ describe('CountdownTimer', () => {
     it('test flow simple', async () => {
         const timer = new CountdownTimer();
         timer.setInitialTime(4000);
-        const onFinish = () => {};
-        timer.onCountdownFinish().subscribe(() => onFinish());
+        const onFinish = jest.fn();
+        const onFinishSubscription = timer.onCountdownFinish().subscribe(() => onFinish());
         timer.run();
 
         // timer should actually be counting down
-        await new Promise((r) => setTimeout(r, 2500));
-        expect(timer.getCurrentTime()).toBe(1500);
+        await new Promise((r) => setTimeout(r, 2555));
+        expectCloseTo(timer.getCurrentTime(), 1500, TIMER_TEST_PRECISION_MS);
 
         // timer should've reached 0 by now
-        await new Promise((r) => setTimeout(r, 1500));
-        expect(timer.getCurrentTime()).toBe(0);
+        await new Promise((r) => setTimeout(r, 1555));
+        expectCloseTo(timer.getCurrentTime(), 0, TIMER_TEST_PRECISION_MS);
 
         // the timer shouldn't count below 0
-        await new Promise((r) => setTimeout(r, 1000));
+        await new Promise((r) => setTimeout(r, 555));
         expect(timer.getCurrentTime()).toBe(0);
 
         // completion event needs to have been called
         // but give it another cycle to be triggered
         await new Promise((r) => setTimeout(r, 1));
         expect(onFinish).toBeCalledTimes(1);
+        onFinishSubscription.unsubscribe();
 
         // timer should return to its initial state
         timer.reset();
@@ -63,43 +71,44 @@ describe('CountdownTimer', () => {
         const timer = new CountdownTimer();
         timer.setInitialTime(1000);
         timer.run();
-        await new Promise((r) => setTimeout(r, 500));
+        await new Promise((r) => setTimeout(r, 555));
 
         // make sure that the test case works properly
-        expect(timer.getCurrentTime()).toBe(500);
+        expectCloseTo(timer.getCurrentTime(), 500, TIMER_TEST_PRECISION_MS);
 
         // the current time should freeze while paused
-        const timeAtPause = timer.getCurrentTime;
+        const timeAtPause = timer.getCurrentTime();
         timer.pause();
-        await new Promise((r) => setTimeout(r, 200));
+        await new Promise((r) => setTimeout(r, 255));
         expect(timer.getCurrentTime()).toBe(timeAtPause);
 
         // the current time should be updating again when resuming
         timer.run();
-        await new Promise((r) => setTimeout(r, 500));
+        await new Promise((r) => setTimeout(r, 555));
         expect(timer.getCurrentTime()).toBe(0);
     })
 
     it('test flow premature reset', async () => {
         const timer = new CountdownTimer();
-        timer.setInitialTime(1000);
+        const initialTime = 1000;
+        timer.setInitialTime(initialTime);
         timer.run();
-        await new Promise((r) => setTimeout(r, 200));
+        await new Promise((r) => setTimeout(r, 255));
 
         // make sure that the test case works properly
-        expect(timer.getCurrentTime()).toBe(800);
+        expectCloseTo(timer.getCurrentTime(), 750, TIMER_TEST_PRECISION_MS);
 
         // the current time needs to be set back to start
         timer.reset();
-        expect(timer.getCurrentTime()).toBe(1000);
+        expect(timer.getCurrentTime()).toBe(initialTime);
 
         // timer should have been paused after reset
-        await new Promise((r) => setTimeout(r, 200));
-        expect(timer.getCurrentTime()).toBe(1000);
+        await new Promise((r) => setTimeout(r, 255));
+        expect(timer.getCurrentTime()).toBe(initialTime);
 
         // the timer should be able to run again after reset
         timer.run();
-        await new Promise((r) => setTimeout(r, 200));
-        expect(timer.getCurrentTime()).toBe(800);
+        await new Promise((r) => setTimeout(r, 255));
+        expectCloseTo(timer.getCurrentTime(), 750, TIMER_TEST_PRECISION_MS);
     })
 })
--- src/CountdownTimer.ts
+++ src/CountdownTimer.ts
@@ -1,37 +1,78 @@
-import { Observable } from 'rxjs';
-import { EMPTY } from "rxjs";
+import { Observable, Subscription, Subject, interval } from 'rxjs';
+
+enum TimerState {
+    Standby,
+    Running,
+    Waiting,
+}
 
 export default class CountdownTimer {
+    private static readonly TIMER_INTERVAL_MS : number = 100;
+
+    private _initialTimeMS : number = 0;
+    private _currentTimeMS : number = 0;
+    private _state : TimerState = TimerState.Standby;
+    private _intervalSubscription? : Subscription = undefined;
+    private readonly _onTimerFinishSubject : Subject<CountdownTimer>;
 
     constructor() {
-        
+        this._onTimerFinishSubject = new Subject<CountdownTimer>();
     }
 
-    setInitialTime(time : Number) : void {
-
+    setInitialTime(milliseconds : number) : void {
+        this._initialTimeMS = milliseconds;
+        if(this._state === TimerState.Standby){
+            this._currentTimeMS = milliseconds;
+        }
     }
 
-    getInitialTime() : Number {
-        return -1;
+    getInitialTime() : number {
+        return this._initialTimeMS;
     }
 
-    getCurrentTime() : Number {
-        return -1;
+    getCurrentTime() : number {
+        return this._currentTimeMS;
     }
 
     run() : void {
-        
+        if(!this.isRunning()){
+            this._intervalSubscription = interval(CountdownTimer.TIMER_INTERVAL_MS)
+                .subscribe(() => this.onTick());
+            this._state = TimerState.Running;
+        }
     }
 
     pause() : void {
-
+        if(this.isRunning()){
+            this._intervalSubscription?.unsubscribe();
+            this._intervalSubscription = undefined;
+            this._state = TimerState.Waiting;
+        }
     }
 
     reset() : void {
-
+        this.pause();
+        this._currentTimeMS = this._initialTimeMS;
+        this._state = TimerState.Standby;
     }
 
-    onCountdownFinish() : Observable<void> {
-        return EMPTY;
+    private isRunning() : boolean {
+        return this._state == TimerState.Running;
+    }
+
+    private onTick(){
+        if(!this.isRunning()) return;
+
+        this._currentTimeMS = Math.max(0, 
+            this._currentTimeMS - CountdownTimer.TIMER_INTERVAL_MS);
+        
+        if(this._currentTimeMS === 0){
+            this.pause();
+            this._onTimerFinishSubject.next(this);
+        }
+    }
+
+    onCountdownFinish() : Observable<CountdownTimer> {
+        return this._onTimerFinishSubject.asObservable();
     }
 }
