58c630063df914e7c898b8a476c7cd52d4d8c487
krysov <>
2021-02-25 14:59:17 +0100

    + added meat to PomodoroTimerViewModel; ~ fixed bugs and local variable retention issue in PomodoroTimerViewModel tests; ~ got tests to pass;

--- src/PomodoroTimerViewModel.test.ts
+++ src/PomodoroTimerViewModel.test.ts
@@ -18,105 +18,120 @@ class DummyStore implements ProfileStoreInterface{
         return 3000;
     }
     getStateIterationsBeforeRecess(): number {
-        return 2;
+        return 3;
+        // the flow should be as follows:
+        // ---------
+        // 0: work
+        // 0: break
+        // ---------
+        // 1: work
+        // 1: break
+        // ---------
+        // 2: work
+        // 2: recess
+        // ---------
+        // 0: back from start
     }
 }
 
+const jestHandler = new JestUnitHandler(true);
+
 describe('PomodoroTimerViewModel', () => {
-    
-    const jestHandler = new JestUnitHandler(false);
 
     async function passPomodoroState(model : PomodoroTimerViewModel, expectedState : PomodoroState, durationMS : number){
         const initialState = model.getPomodoroState();
         expect(initialState).toBe(expectedState);
-        console.log("- entering pomodoro state: " + initialState);
 
-        const onChangeState = jest.fn((_model : PomodoroStateChangeInterface) => {
-            // the new state should be a different one now
-            const newState = _model.getPomodoroState();
-            expect(newState).not.toBe(expectedState);
-            console.log("- passed pomodoro state to: " + newState);
+        // subscribe to state changes 
+        let onChangeState : ((m:PomodoroStateChangeInterface) => void)|undefined
+            = jest.fn((_model : PomodoroStateChangeInterface) => {
+                // the new state should be a different one now
+                const newState = _model.getPomodoroState();
+                expect(newState).not.toBe(initialState);
         });
         const onChangeStateSubscription = model.onPomodoroStateChange()
-            .subscribe(_model => onChangeState(_model));
-        model.run();
-
-        // make timer of the current state run out
-        await jestHandler.delay(durationMS + 5);
+            .subscribe(_model => {if(onChangeState!==undefined)onChangeState(_model)});
         
-        // update should have been called by now
+        // make timer of the current state run out
+        model.run();
+        await jestHandler.delay(durationMS);
+        
+        // update should have been called by now exactly once
         expect(onChangeState).toBeCalledTimes(1);
+        onChangeState = undefined; // typescript requires me to clear this local var
         onChangeStateSubscription.unsubscribe;
     }
 
     it('test flow normal', async () => {
         jestHandler.beginUnitTest();
-        console.log("testing pomodoro flow normal:");
         const store = new DummyStore();
         const model = new PomodoroTimerViewModel(store);
 
         // initial state should be "work"
-        for(var i:number = 1; i <= store.getStateIterationsBeforeRecess(); ++i){
+        for(var i:number = 1; i < store.getStateIterationsBeforeRecess(); ++i){
             await passPomodoroState(model, PomodoroState.Work, store.getStateDurationWork());
             await passPomodoroState(model, PomodoroState.Break, store.getStateDurationBreak());
         }
-        await passPomodoroState(model, PomodoroState.Recess, store.getStateDurationRecess());
         await passPomodoroState(model, PomodoroState.Work, store.getStateDurationWork());
-        expect(model.getPomodoroState()).toBe(PomodoroState.Break);
+        await passPomodoroState(model, PomodoroState.Recess, store.getStateDurationRecess());
+        expect(model.getPomodoroState()).toBe(PomodoroState.Work);
 
         jestHandler.finishUnitTest();
     })
 
     it('test flow interruption', async () => {
         jestHandler.beginUnitTest();
-        console.log("testing pomodoro flow with interruptions:");
         const store = new DummyStore();
         const model = new PomodoroTimerViewModel(store);
 
+        // initial state should be "work"
         expect(model.getPomodoroState()).toBe(PomodoroState.Work);
         model.run();
         expect(model.isRunning()).toBe(true);
         await jestHandler.delay(155);
+
+        model.pause();
+        expect(model.isRunning()).toBe(false);
+        await jestHandler.delay(155);
+
+        model.run();
+        expect(model.isRunning()).toBe(true);
+        await jestHandler.delay(155);
+
         model.reset();
         expect(model.isRunning()).toBe(false);
         expect(model.getPomodoroState()).toBe(PomodoroState.Work);
+        await jestHandler.delay(155);
+
+        model.run();
+        expect(model.isRunning()).toBe(true);
+
         jestHandler.finishUnitTest();
     })
 
     it('test flow skip state', async () => {
         jestHandler.beginUnitTest();
-        console.log("testing pomodoro flow with skips:");
         const store = new DummyStore();
         const model = new PomodoroTimerViewModel(store);
 
         // initial state should be "work"
-        console.log("- starting with state:" + model.getPomodoroState());
         expect(model.getPomodoroState()).toBe(PomodoroState.Work);
         model.skipState();
-        console.log("- skipped to state:" + model.getPomodoroState());
         expect(model.getPomodoroState()).toBe(PomodoroState.Break);
         model.skipState();
-        console.log("- skipped to state:" + model.getPomodoroState());
         expect(model.getPomodoroState()).toBe(PomodoroState.Work);
 
         // skip should work while timer is still running
         model.run();
         await jestHandler.delay(155);
         model.skipState();
-        console.log("- skipped to state during run:" + model.getPomodoroState());
         expect(model.getPomodoroState()).toBe(PomodoroState.Break);
-        model.run();
-        await jestHandler.delay(155);
-        model.skipState();
-        console.log("- skipped to state during run:" + model.getPomodoroState());
-        expect(model.getPomodoroState()).toBe(PomodoroState.Recess);
 
         jestHandler.finishUnitTest();
     })
 
     it('test time output', async () => {
         jestHandler.beginUnitTest();
-        console.log("testing time output:");
         const store = new DummyStore();
         var expectedMinutes = 25;
         var expectedSeconds = 0;
@@ -127,15 +142,14 @@ describe('PomodoroTimerViewModel', () => {
 
         expect(model.getCurrentTimePartMinutes()).toBe(expectedMinutes);
         expect(model.getCurrentTimePartSeconds()).toBe(expectedSeconds);
-        console.log("time output: " + expectedMinutes + " : " + expectedSeconds);
 
         // let 15 seconds pass from 25:00 to 24:45
+        model.run();
         await jestHandler.delay(15000);
         expectedMinutes = 24;
         expectedSeconds = 45;
         expect(model.getCurrentTimePartMinutes()).toBe(expectedMinutes);
         expect(model.getCurrentTimePartSeconds()).toBe(expectedSeconds);
-        console.log("time output: " + expectedMinutes + " : " + expectedSeconds);
 
         jestHandler.finishUnitTest();
     })
--- src/PomodoroTimerViewModel.ts
+++ src/PomodoroTimerViewModel.ts
@@ -1,38 +1,108 @@
 import ProfileStoreInterface from "./ProfileStoreInterface"
 import PomodoroStateChangeInterface, { PomodoroState } from "./PomodoroStateChangeInterface";
-import { Observable } from "rxjs";
+import { Observable, Subject, timer } from "rxjs";
+import CountdownTimer from "./CountdownTimer";
 
 export default class PomodoroTimerViewModel implements PomodoroStateChangeInterface{
 
+    private readonly _timer : CountdownTimer;
+    private readonly _store : ProfileStoreInterface;
+    private readonly _onPomodoroStateChange : Subject<PomodoroStateChangeInterface>;
+
+    private _state : PomodoroState;
+    private _workIterations : number;
+    private _isRunning : boolean = false;
+
     constructor(store : ProfileStoreInterface) {
-        
+        this._onPomodoroStateChange = new Subject();
+        this._timer = new CountdownTimer();
+        this._timer.onCountdownFinish()
+            .subscribe(timer => this.gotoNextState());
+        this._store = store;
+
+        this._workIterations = 1;
+        this._state = PomodoroState.Work;
+        this.setupTimerForState(this._state);
     }
 
     getPomodoroState(): PomodoroState {
-        throw new Error("Method not implemented.");
+        return this._state;
     }
 
     onPomodoroStateChange(): Observable<PomodoroStateChangeInterface> {
-        throw new Error("Method not implemented.");
+        return this._onPomodoroStateChange.asObservable();
     }
 
     getCurrentTimePartSeconds() : number {
-        return -1;
+        const milliseconds = this._timer.getCurrentTime();
+        return Math.floor(milliseconds/1000) % 60;
     }
 
     getCurrentTimePartMinutes() : number {
-        return -1;
+        const milliseconds = this._timer.getCurrentTime();
+        return Math.floor(milliseconds/60000);
     }
 
     isRunning() : boolean {
-        throw new Error("Method not implemented.");
+        return this._isRunning;
     }
 
-    run(){}
+    run(){
+        this._isRunning = true;
+        this._timer.run();
+    }
 
-    pause(){}
+    pause(){
+        this._isRunning = false;
+        this._timer.pause();
+    }
 
-    reset(){}
+    reset(){
+        this._isRunning = false;
+        this._timer.reset();
+    }
 
-    skipState(){}
+    skipState(){
+        this.gotoNextState();
+    }
+
+    private gotoNextState(){
+        const initialState = this._state;
+        var nextState : PomodoroState;
+        if(initialState === PomodoroState.Work){
+            if(this._workIterations >= this._store.getStateIterationsBeforeRecess()){
+                nextState = PomodoroState.Recess;
+            }else nextState = PomodoroState.Break;
+        }else{
+            nextState = PomodoroState.Work;
+        }
+        if(nextState === PomodoroState.Work){
+            ++this._workIterations;
+        }else if(nextState === PomodoroState.Recess){
+            this._workIterations = 0;
+        }
+        this.setState(nextState);
+    }
+
+    private setState(state : PomodoroState){
+        const didChange = this._state !== state;
+        this._state = state;
+        this.setupTimerForState(state);
+        if(didChange) this._onPomodoroStateChange.next(this);
+    }
+
+    private setupTimerForState(state : PomodoroState){
+        this._timer.reset();
+        switch(state){
+            case PomodoroState.Work:
+                this._timer.setInitialTime(this._store.getStateDurationWork());
+                break;
+            case PomodoroState.Break:
+                this._timer.setInitialTime(this._store.getStateDurationBreak());
+                break;
+            case PomodoroState.Recess:
+                this._timer.setInitialTime(this._store.getStateDurationRecess());
+                break;
+        }
+    }
 }
